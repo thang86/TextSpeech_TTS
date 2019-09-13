b53806d0891a590c78836116878f1b5e4ea53654
thang86 <tranthang8696@gmail.com>
2019-09-13 22:08:33 +0700

    init project

---
+++ app/src/main/java/io/github/thang86/textspeech_tts/TextSpeechActivity.java
@@ -0,0 +1,13 @@
+package io.github.thang86.textspeech_tts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TextSpeechActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_speech);
+    }
+}
